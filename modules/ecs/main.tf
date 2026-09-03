764e84efb83a30a89325cc7449dcaba46b070eb9
LucasJdn <lucasjdniyczuk2018@gmail.com>
2026-09-03 09:11:06 -0300

    add ECS task definition with container configurations

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -38,3 +38,36 @@ resource "aws_iam_role" "task_role" {
     }]
   })
 }
+
+
+resource "aws_ecs_task_definition" "task_test" {
+  family                   = "..."
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.execution_role.arn
+  task_role_arn            = aws_iam_role.task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "..."
+      image     = "${aws_ecr_repository.jdn_repo.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "..."
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
